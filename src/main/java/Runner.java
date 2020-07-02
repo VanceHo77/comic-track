4e7b5eb66cc5a5fabb9a81c4f7dd4cf9349b2090
Vance Ho <gn667226@outlook.com>
2020-07-02 21:16:24 +0800

    移除 鬼滅之刃

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -7,64 +7,59 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class Runner {
-    public static void main(String[] args) throws IOException {
-        final String propertiesPath = getPropertiesPath();
-        File propertiesFile = new File(propertiesPath);
-        if(!propertiesFile.exists()){
-            propertiesFile.createNewFile();
-        }
-        Properties properties = PropertiesAccessHelper.read(propertiesPath);
+  public static void main(String[] args) throws IOException {
+    final String propertiesPath = getPropertiesPath();
+    File propertiesFile = new File(propertiesPath);
+    if (!propertiesFile.exists()) {
+      propertiesFile.createNewFile();
+    }
+    Properties properties = PropertiesAccessHelper.read(propertiesPath);
 
-        String comicName = "鬼滅之刃";
-        String baseKukuUrl = "http://comic.ikkdm.com";
-        BaseCrawler kuku鬼滅之刃 = new KukuCrawler(baseKukuUrl +"/comiclist/2126/");
-        BaseCrawler.Result result = kuku鬼滅之刃.getLastedEp();
-        boolean isLastedEp = isLastedEp(properties.getProperty(comicName), result);
-        System.out.println(comicName + " is lasted:" + isLastedEp);
-        if (isLastedEp) {
-            System.out.println(result);
-            properties.put(comicName, String.valueOf(result.getLastedEpNumber()));
-        }
-
-        comicName = "海賊王";
-        BaseCrawler kuku海賊王 = new KukuCrawler(baseKukuUrl + "/comiclist/4/");
-        result = kuku海賊王.getLastedEp();
-        isLastedEp = isLastedEp(properties.getProperty(comicName), result);
-        System.out.println(comicName + "is lasted:" + isLastedEp);
-        if (isLastedEp) {
-            System.out.println(result);
-            properties.put(comicName, String.valueOf(result.getLastedEpNumber()));
-        }
-
-        comicName = "王者天下";
-        BaseCrawler 漫畫人_王者天下 = new ManhuaguiCrawler("http://www.dm5.com", "manhua-wangzhetianxia");
-        result = 漫畫人_王者天下.getLastedEp();
-        isLastedEp = isLastedEp(properties.getProperty(comicName), result);
-        System.out.println(comicName + " is lasted:" + isLastedEp);
-        if (isLastedEp) {
-            System.out.println(result);
-            properties.put(comicName, String.valueOf(result.getLastedEpNumber()));
-        }
-
-
-        writeLastedEp(properties, propertiesPath);
+    String comicName = "海賊王";
+    String baseKukuUrl = "http://comic.ikkdm.com";
+    BaseCrawler kuku海賊王 = new KukuCrawler(baseKukuUrl + "/comiclist/4/");
+    BaseCrawler.Result result;
+    boolean isLastedEp;
+    try {
+      result = kuku海賊王.getLastedEp();
+      isLastedEp = isLastedEp(properties.getProperty(comicName), result);
+      System.out.println(comicName + "is lasted:" + isLastedEp);
+      if (isLastedEp) {
+        System.out.println(result);
+        properties.put(comicName, String.valueOf(result.getLastedEpNumber()));
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
     }
 
-    private static boolean isLastedEp(String propEpNumber, BaseCrawler.Result result) {
-        final Integer lastedEpNumber = result.getLastedEpNumber();
-        if (null == propEpNumber) {
-            return true;
-        }
-        return Integer.parseInt(propEpNumber) < lastedEpNumber;
+    comicName = "王者天下";
+    BaseCrawler 漫畫人_王者天下 = new ManhuaguiCrawler("http://www.dm5.com", "manhua-wangzhetianxia");
+    result = 漫畫人_王者天下.getLastedEp();
+    isLastedEp = isLastedEp(properties.getProperty(comicName), result);
+    System.out.println(comicName + " is lasted:" + isLastedEp);
+    if (isLastedEp) {
+      System.out.println(result);
+      properties.put(comicName, String.valueOf(result.getLastedEpNumber()));
     }
 
-    private static String getPropertiesPath() {
-        final File currentDirFile = new File(".");
-        final String projectPath = currentDirFile.getAbsoluteFile().getParent();
-        return projectPath + "/storeLastedEpInfo.properties";
-    }
+    writeLastedEp(properties, propertiesPath);
+  }
 
-    private static void writeLastedEp(Properties properties, String propPath) throws IOException {
-        PropertiesAccessHelper.write(properties, propPath);
+  private static boolean isLastedEp(String propEpNumber, BaseCrawler.Result result) {
+    final Integer lastedEpNumber = result.getLastedEpNumber();
+    if (null == propEpNumber) {
+      return true;
     }
+    return Integer.parseInt(propEpNumber) < lastedEpNumber;
+  }
+
+  private static String getPropertiesPath() {
+    final File currentDirFile = new File(".");
+    final String projectPath = currentDirFile.getAbsoluteFile().getParent();
+    return projectPath + "/storeLastedEpInfo.properties";
+  }
+
+  private static void writeLastedEp(Properties properties, String propPath) throws IOException {
+    PropertiesAccessHelper.write(properties, propPath);
+  }
 }
